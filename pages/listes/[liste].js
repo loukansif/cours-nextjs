1a7d6b220b99f9b92f8c4e80fd23dbe0f655dd7c
loukansif <shakbaz@gmail.com>
2022-09-06 17:38:38 +0200

    getStaticPaths

--- pages/listes/[liste].js
+++ pages/listes/[liste].js
@@ -1,15 +1,30 @@
+import styles from "/styles/Home.module.css"
 import React from 'react'
+import { v4 as uuidv4 } from "uuid";
 
-export default function liste() {
+export default function liste(props) {
+  console.log(props);
   return (
-    <div>Hello liste</div>
+    <div className={styles.container}>
+    <h1 className={styles.titre}>Vocabulaire de base</h1>
+    <table className={styles.tableau}>
+      <tbody>
+        {props.listeEnCours.map(el => (
+          <tr key={uuidv4()}>
+            <td>{el.en}</td>
+            <td>{el.fr}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
   )
 }
 
 
 export async function getStaticProps(context){
   const slug = context.params.liste;
-  const data = await import('/data/liste.json')
+  const data = await import('/data/listes.json')
 
   const listeEnCours = data.englishList.find(list => list.name === slug)
   return {
@@ -21,10 +36,12 @@ export async function getStaticProps(context){
 
 export async function getStaticPaths(){
   const data = await import('/data/listes.json')
+  const paths = await data.englishList.map(item => ({
+    params: {liste: item.name}
+  }))
+
   return {
-    paths: [
-      {params: {liste: 'words'}}
-    ],
+    paths,
     fallback: false
   }
 }
